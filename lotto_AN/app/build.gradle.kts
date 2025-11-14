18b336a807e87c25dc298ad370a0c984f41079bc
baekcii <bsy8470@naver.com>
2025-11-14 23:08:10 +0900

    feat: retrofit 설정 및 LottoApi 추가
    - Retrofit 기반 LottoApi 인터페이스 구현
    - ApiModule 추가하여 retrofit,lottoApi 의존성 제공

--- lotto_AN/app/build.gradle.kts
+++ lotto_AN/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -57,4 +59,15 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //coroutine
+    implementation(libs.kotlinx.coroutines.android)
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
+
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 }
---
+++ lotto_AN/app/src/main/java/com/baek/lotto/data/remote/LottoApi.kt
@@ -0,0 +1,26 @@
+package com.baek.lotto.data.remote
+
+import com.baek.lotto.common.ApiResponse
+import com.baek.lotto.data.model.DrawDto
+import com.baek.lotto.data.model.RandomLottoDto
+import com.baek.lotto.data.model.SyncResultDto
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface LottoApi {
+
+    @POST("api/draw/sync")
+    suspend fun sync(): ApiResponse<SyncResultDto>
+
+    @GET("api/draw/{drwNo}")
+    suspend fun getDraw(
+        @Path("drwNo") drwNo: Int
+    ): ApiResponse<DrawDto>
+
+    @GET("api/draw/latest")
+    suspend fun getLatestDraw(): ApiResponse<DrawDto>
+
+    @POST("api/lotto/random")
+    suspend fun createRandomLotto(): ApiResponse<RandomLottoDto>
+}
