d5ba1aea46faf0be31344425434d385380fe3819
baekcii <bsy8470@naver.com>
2025-11-18 22:05:12 +0900

    feat(di): StorageRepository 의존성 주입 추가

--- lotto_AN/app/src/main/java/com/baek/lotto/di/RepositoryModule.kt
+++ lotto_AN/app/src/main/java/com/baek/lotto/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.baek.lotto.di
 
 import com.baek.lotto.data.repository.LottoRepositoryImpl
+import com.baek.lotto.data.repository.StorageRepositoryImpl
 import com.baek.lotto.domain.repository.LottoRepository
+import com.baek.lotto.domain.repository.StorageRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -16,4 +18,10 @@ abstract class RepositoryModule {
     abstract fun bindLottoRepository(
         lottoRepositoryImpl: LottoRepositoryImpl
     ): LottoRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindStorageRepository(
+        storageRepositoryImpl: StorageRepositoryImpl
+    ): StorageRepository
 }
