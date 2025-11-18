24f9af1ea5cd2d63362f48afe8d335a6af48aaa4
baekcii <bsy8470@naver.com>
2025-11-18 22:02:03 +0900

    feat(repository): 로또 기록 저장을 위한 StorageRepository추가
    - StorageRepository 인터페이스 및 구현체 추가
    - 로또 기록 저장, 조회, 삭제 기능 구현

---
+++ lotto_AN/app/src/main/java/com/baek/lotto/data/repository/StorageRepositoryImpl.kt
@@ -0,0 +1,51 @@
+package com.baek.lotto.data.repository
+
+import com.baek.lotto.common.Result
+import com.baek.lotto.data.local.dao.LottoRecordDao
+import com.baek.lotto.data.mapper.EntityMapper.toDomain
+import com.baek.lotto.data.mapper.EntityMapper.toEntity
+import com.baek.lotto.domain.model.LottoRecord
+import com.baek.lotto.domain.model.RandomLotto
+import com.baek.lotto.domain.repository.StorageRepository
+import java.time.LocalDate
+import javax.inject.Inject
+
+class StorageRepositoryImpl @Inject constructor(
+    private val dao: LottoRecordDao
+) : StorageRepository {
+    override suspend fun saveLotto(lottos: List<RandomLotto>): Result<Unit> {
+        return handleDao {
+            val now = LocalDate.now()
+            val records = lottos.map { it.toEntity(now) }
+
+            dao.insertAll(records)
+        }
+    }
+
+    override suspend fun getLottoHistory(): Result<List<LottoRecord>> {
+        return handleDao {
+            val result = dao.getAll()
+            result.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun deleteLotto(id: Long): Result<Unit> {
+        return handleDao {
+            dao.delete(id)
+        }
+    }
+
+    override suspend fun deleteAll(): Result<Unit> {
+        return handleDao {
+            dao.clear()
+        }
+    }
+
+    private inline fun <T> handleDao(block: () -> T): Result<T> {
+        return try {
+            Result.Success(block())
+        } catch (e: Exception) {
+            Result.Error(e.message, e)
+        }
+    }
+}
---
+++ lotto_AN/app/src/main/java/com/baek/lotto/domain/repository/StorageRepository.kt
@@ -0,0 +1,16 @@
+package com.baek.lotto.domain.repository
+
+import com.baek.lotto.common.Result
+import com.baek.lotto.domain.model.LottoRecord
+import com.baek.lotto.domain.model.RandomLotto
+
+interface StorageRepository {
+
+    suspend fun saveLotto(lottos: List<RandomLotto>): Result<Unit>
+
+    suspend fun getLottoHistory(): Result<List<LottoRecord>>
+
+    suspend fun deleteLotto(id: Long): Result<Unit>
+
+    suspend fun deleteAll(): Result<Unit>
+}
